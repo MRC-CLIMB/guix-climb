e112c9a3c4fe90aad4644ae0e793e679c7efbf76
Marius Bakke <mbakke@fastmail.com>
2016-08-14 02:16:39 +0100

    kraken: fix license

--- climb/packages/bioinformatics.scm
+++ climb/packages/bioinformatics.scm
@@ -507,13 +507,8 @@ assemblies or reads.")
     (home-page "http://ccb.jhu.edu/software/kraken/")
     (synopsis "Taxonomic sequence classification system")
     (description "Kraken is a system for assigning taxonomic labels to short DNA
-sequences, usually obtained through metagenomic studies.  Previous attempts by other
-bioinformatics software to accomplish this task have often used sequence alignment or
-machine learning techniques that were quite slow, leading to the development of less
-sensitive but much faster abundance estimation programs.  Kraken aims to achieve high
-sensitivity and high speed by utilizing exact alignments of k-mers and a novel
-classification algorithm.")
-    (license license:gpl3)))
+sequences, usually obtained through metagenomic studies.")
+    (license license:gpl3+)))
 
 (define-public srst2
   (package
