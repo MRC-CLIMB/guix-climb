469c2b445c6c725f50ff17765ec13200fbfeea01
Marius Bakke <mbakke@fastmail.com>
2016-09-06 20:46:15 +0100

    drop upstreamed gzstream

--- climb/packages/bioinformatics.scm
+++ climb/packages/bioinformatics.scm
@@ -23,7 +23,6 @@
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system python)
-  #:use-module (climb packages compression)
   #:use-module (climb packages python)
   #:use-module (gnu packages)
   #:use-module (gnu packages autotools)
